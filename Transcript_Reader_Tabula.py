0fa3d43b5d23375ca2bdb857f25f903c8901854e
Dwayne232001 <67543073+Dwayne232001@users.noreply.github.com>
2021-06-04 03:09:43 +0530

    Replaced the deleted file

---
+++ Transcript_Reader_Tabula.py
@@ -0,0 +1,17 @@
+import csv
+import tabula
+file = "Sample_Transcript_1.pdf"
+tables = tabula.read_pdf(file,pages=1,multiple_tables=True)
+x=0
+"""To convert into a text file"""
+# try: 
+#     with open("Transcript2.txt", "a", newline="") as file:
+#         while x<8:
+#             file.write(str(tables[x]))
+#             x=x+1
+# except OSError: 
+#     print('Failed creating the file') 
+# else: 
+#     print('File created')
+"""To convert into a csv file""" 
+tabula.convert_into(file, "Transcript.csv", output_format="csv", pages='all')
